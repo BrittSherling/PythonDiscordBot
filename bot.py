c9f5833fa0b4bd200ba65528988392c80c6d69d6
Britt Sherling <bsherlinggit@gmail.com>
2017-02-07 23:13:40 -0600

    added voice client, and toMe command
    the bot can now connect to voice channels and be moved around with the
    toMe command

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 import discord
-from discord import User
 from discord.ext.commands import Bot
 import secrets
 import requests
 import logging
 
-logging.basicConfig(level=logging.INFO)
 pybot = Bot(command_prefix = "!")
 client = discord.Client
+discord.opus.load_opus
+logging.basicConfig(level=logging.INFO)
 
 @pybot.command()
 async def hello():
@@ -21,10 +21,23 @@ async def truck(user_id):
 async def lenny():
     return await pybot.say('( ͡° ͜ʖ ͡°)')
 
-@pybot.command()
+@pybot.command(aliases=['s'])
 async def search(*q):
     query = {'q': ' '.join(q)}
     respon = requests.get('http://duckduckgo.com/', params=query).url
     return await pybot.say(respon)
+
+@pybot.command(pass_context=True, aliases=['tm'])
+async def toMe(context):
+    author = context.message.author
+    author_id = author.id
+    server = author.server
+    channels = server.channels
+    if discord.opus.is_loaded:
+        for channel in channels:
+            for member in channel.voice_members:
+                if member.id == author_id:
+                    await pybot.join_voice_channel(channel=channel)
+    pybot.say('opus voice not loaded')
 
 pybot.run(secrets.BOT_TOKEN)
